65bbbb170124ea8966e00a75b5dad1e7afc21860
Guillem Pages <guillem@guillempages.cat>
2014-07-06 22:07:18 +0200

    Fix convenience commands
    Add support for RGB and new PCB pin design.

--- GccApplication2/commands.h
+++ GccApplication2/commands.h
@@ -6,11 +6,11 @@
 #define CMD_INIT_1        0x05   //This command will be forwarded as such; can be used to check the number of slaves.
 #define CMD_INIT_2        0x0C   //This command will be forwarded as such; can be used to check the number of slaves.
 
-#define CMD_LED_FADE      0x20   //Fade the LED slowly on or off, depending on the last bit.
-#define CMD_LED_SET       0x30   //Commands to switch the LED on or off, depending on the last bit.
-
 #define CMD_READ_SENSOR   0x10   //Read the current value of the desired ADC converter. Last 4 bits reserved for indexing.
-#define CMD_CHECK_SENSOR  0x60   //Compare the sensor with the current threshold; return 0 if lower, 0xFF if higher. Last 4 bits reserved for indexing.
+
+#define CMD_LED_FADE      0x20   //Fade the LED slowly on or off. Last 4 bits specify the color (WRGB).
+#define CMD_LED_SET       0x30   //Commands to switch the LED on or off. Last 4 bits specify the color (WRGB)
+
 #define CMD_GET_THRESHOLD 0x50   //Get the current threshold for the given sensor. Last 4 bits reserved for indexing.
 #define CMD_SET_THRESHOLD 0x40   //Set the current threshold for the given sensor. Last 4 bits reserved for indexing. 
                                       //The next "executable command" will be interpreted as the threshold and not executed.
@@ -19,14 +19,14 @@
 #define CMD_PARAM_MASK    0x0F   // Mask used to separate parameter from the command.
 
 /* Convenience commands; with parameter included */
-#define CMD_LED_SET_OFF   CMD_LED_SET        //Switch the LED off, regardless of the current status.
-#define CMD_LED_SET_ON    (CMD_LED_SET | 1)  //Switch the LED on, regardless of the current status.
+#define CMD_LED_SET_OFF   CMD_LED_SET        //Switch the LEDs off, regardless of the current status.
+#define CMD_LED_SET_ON    (CMD_LED_SET | 0x0F)  //Switch all LEDs on, regardless of the current status.
 
-#define CMD_LED_FADE_OFF  CMD_LED_FADE       //Fade the LED slowly off.
-#define CMD_LED_FADE_ON   (CMD_LED_FADE | 1) //Fade the LED slowly on.
+#define CMD_LED_FADE_OFF  CMD_LED_FADE       //Fade the LEDs slowly off.
+#define CMD_LED_FADE_ON   (CMD_LED_FADE | 0x0F) //Fade all LEDs slowly on.
 
 #define CMD_GET_STEP        (CMD_READ_SENSOR | 2) // Get the value of the (step) sensor ADC converter (ADC 2)
-#define CMD_GET_LIGHT       (CMD_READ_SENSOR | 0) //Get the value of the (light) sensor ADC converter (ADC 0)
+#define CMD_GET_LIGHT       (CMD_READ_SENSOR | 7) //Get the value of the (light) sensor ADC converter (ADC 7)
 #define CMD_GET_TEMPERATURE (CMD_READ_SENSOR | 8) //Get the value of the internal temperature sensor (ADC 8)
 
 
